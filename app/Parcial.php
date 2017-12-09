101b03678ff1f8e6ac226ffd24213c5416a06f27
SoranVB <soranvibritannia@hotmail.com>
2017-12-09 05:50:45 -0700

    asignaturas

--- app/Parcial.php
+++ app/Parcial.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Parcial extends Model
 {
+
+			use SoftDeletes;
              public function alumno()
         {
             return $this->hasMany(Alumno::class, 'alumno_id', 'asignatura_id');
